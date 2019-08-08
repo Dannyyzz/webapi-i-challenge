030eb88e22497ef94c912e19faf4532a9d166795
Daniel Viera <dannyzz@live.com>
2019-08-07 23:19:03 -0400

    POST for /api/users

--- index.js
+++ index.js
@@ -9,6 +9,37 @@ const server = express();
 //Middleware
 server.use(express.json());
 
+//request handlers
+server.post("/api/users", (req, res) => {
+  const { name, bio } = req.body;
+  if (!name || !bio) {
+    res.status(400).json({
+      errorMessage: "Please provide name and bio for the user."
+    });
+    return;
+  }
+
+  db.insert({ name, bio })
+    .then(({ id }) => {
+      db.findById(id)
+        .then(user => {
+          res.status(201).json({
+            user
+          });
+        })
+        .catch(() => {
+          res.status(500).json({
+            error: "There was an error while saving the user to the database"
+          });
+        });
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "There was an error while saving the user to the database"
+      });
+    });
+});
+
 //should always be last
 server.listen(1337, () => {
   console.log("Server is running on port 1337...");
