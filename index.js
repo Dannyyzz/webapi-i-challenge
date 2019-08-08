b4a46d0b5e472232d9a52cf63c3c017cb054402a
Daniel Viera <dannyzz@live.com>
2019-08-07 23:15:50 -0400

    Server setup && dependancies

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // implement your API here
+
+// Libraries
+const express = require("express");
+// Misc Files
+const db = require("./data/db.js");
+// Global Objects
+const server = express();
+//Middleware
+server.use(express.json());
+
+//should always be last
+server.listen(1337, () => {
+  console.log("Server is running on port 1337...");
+});
